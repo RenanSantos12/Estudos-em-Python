dadb4367a27fe371b4d65ce3302809de0cd1656a
Renan <renansantos140406@gmail.com>
2024-10-25 08:44:08 -0300

    if, elif, else

--- Aula_input.py
+++ Aula_input.py
@@ -1,1 +1,14 @@
-print
+
+
+# operação condicionais 
+
+entrada = input('Voce quer entrar ou sair do sitema : ')
+
+if entrada == 'entrar':
+    print('VOcê entrou no sistema')
+
+elif entrada == 'Sair':
+    print('Você saiu do sitema')
+
+else:
+    print('você não digitou nem um nem outro')        
