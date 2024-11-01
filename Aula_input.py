2d2f2c4828d6063420b9f5dee1737b0020a70fe2
Renan <renansantos140406@gmail.com>
2024-11-01 13:22:16 -0300

    aaaa

--- Aula_input.py
+++ Aula_input.py
@@ -1,14 +1,33 @@
-
-
 # operação condicionais 
-
 entrada = input('Voce quer entrar ou sair do sitema : ')
 
 if entrada == 'entrar':
     print('VOcê entrou no sistema')
 
+
+# elif é a segunda condição. se não se...
 elif entrada == 'Sair':
     print('Você saiu do sitema')
 
 else:
     print('você não digitou nem um nem outro')        
+
+condicao1 = True    
+condicao2 = True    
+condicao3 = True    
+condicao4 = True    
+
+if condicao1:
+    print('condição 1')
+
+elif condicao2:
+    print('condição 2')
+
+elif condicao2:
+    print('condição 3')
+
+elif condicao2:
+    print('condição 4')
+
+else:
+    print('tudo errado')     
