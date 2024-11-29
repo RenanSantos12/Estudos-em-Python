79eeebc9ffcd91da1d2c2fb65ea4a57fd87e438e
Renan <renansantos140406@gmail.com>
2024-11-29 13:36:50 -0300

    desafiio

--- aula77_for.py
+++ aula77_for.py
@@ -1,6 +1,20 @@
-digitacao = 'Renan lindo'
+'''digitacao = 'Renan lindo'
 
 for caracter in digitacao:
     print(caracter)
 
-    
+    '''
+
+palavra_secreta = 'perfume'
+letras_acertadas = ''
+while True:
+    letra_digitada = input('Digite uma letra: ')
+
+    if len(letra_digitada) > 1:
+        print('Digite apenas uma letra')
+        continue
+
+    if letra_digitada in palavra_secreta:
+        letras_acertadas += letras_acertadas
+
+        
